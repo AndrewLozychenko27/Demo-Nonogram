4b54d550533bed3446fa8d84f4018e43b70b612a
Andrii Lozychenko <andrew.lozychenko@gmail.com>
2024-01-11 10:49:05 +0200

    Project init

---
+++ src/test/java/ua/lozychenko/nonogram/NonogramApplicationTests.java
@@ -0,0 +1,11 @@
+package ua.lozychenko.nonogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonogramApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
