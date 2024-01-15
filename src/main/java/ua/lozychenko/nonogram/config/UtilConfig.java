e1d97e57ec37a694b780851b165f975eb2d2da20
Andrii Lozychenko <andrew.lozychenko@gmail.com>
2024-01-15 12:04:49 +0200

    Application: Created Util Config

---
+++ src/main/java/ua/lozychenko/nonogram/config/UtilConfig.java
@@ -0,0 +1,18 @@
+package ua.lozychenko.nonogram.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+public class UtilConfig {
+    @Bean
+    public ViewResolver viewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(true);
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        return resolver;
+    }
+}
