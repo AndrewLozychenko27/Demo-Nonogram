49b4580f63b8d4573218fa7e9bcacb923d3d9a67
Andrii Lozychenko <andrew.lozychenko@gmail.com>
2024-01-17 12:02:50 +0200

    Data: added Service annotation

--- src/main/java/ua/lozychenko/nonogram/data/service/RoleService.java
+++ src/main/java/ua/lozychenko/nonogram/data/service/RoleService.java
@@ -1,6 +1,8 @@
 package ua.lozychenko.nonogram.data.service;
 
+import org.springframework.stereotype.Service;
 import ua.lozychenko.nonogram.data.entity.Role;
 
+@Service
 public interface RoleService extends BaseService<Role> {
 }
