e1e068ba7caeff151579ccd98723f3ee0f37ed08
Andrii Lozychenko <andrew.lozychenko@gmail.com>
2024-01-19 12:09:33 +0200

    Data: removed Role table

--- src/main/java/ua/lozychenko/nonogram/data/service/RoleService.java
+++
@@ -1,8 +0,0 @@
-package ua.lozychenko.nonogram.data.service;
-
-import org.springframework.stereotype.Service;
-import ua.lozychenko.nonogram.data.entity.Role;
-
-@Service
-public interface RoleService extends BaseService<Role> {
-}
