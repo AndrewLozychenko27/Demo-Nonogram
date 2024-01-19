952ad5d7394427cffb300e175ae0d86ed566a07e
Andrii Lozychenko <andrew.lozychenko@gmail.com>
2024-01-19 12:12:47 +0200

    Data: Removed annotation

--- src/main/java/ua/lozychenko/nonogram/data/service/UserService.java
+++ src/main/java/ua/lozychenko/nonogram/data/service/UserService.java
@@ -1,11 +1,9 @@
 package ua.lozychenko.nonogram.data.service;
 
-import org.springframework.stereotype.Service;
 import ua.lozychenko.nonogram.data.entity.User;
 
 import java.util.Optional;
 
-@Service
 public interface UserService extends BaseService<User> {
     Optional<User> findByNickname(String nickname);
 }
