34cb82b04486118737bc30718877cea8cf4578b4
Andrii Lozychenko <andrew.lozychenko@gmail.com>
2024-01-19 12:10:26 +0200

    Data: overrided add and edit

--- src/main/java/ua/lozychenko/nonogram/data/service/impl/DefaultUserService.java
+++ src/main/java/ua/lozychenko/nonogram/data/service/impl/DefaultUserService.java
@@ -3,7 +3,9 @@ package ua.lozychenko.nonogram.data.service.impl;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import ua.lozychenko.nonogram.data.entity.Role;
 import ua.lozychenko.nonogram.data.entity.User;
 import ua.lozychenko.nonogram.data.repo.UserRepo;
 import ua.lozychenko.nonogram.data.service.UserService;
@@ -12,11 +14,31 @@ import java.util.Optional;
 
 @Service
 public class DefaultUserService extends DefaultBaseService<User> implements UserService, UserDetailsService {
-    private UserRepo repo;
+    private final UserRepo repo;
+    private final PasswordEncoder encoder;
 
-    public DefaultUserService(UserRepo repo) {
+    public DefaultUserService(UserRepo repo, PasswordEncoder encoder) {
         super(repo);
         this.repo = repo;
+        this.encoder = encoder;
+    }
+
+    @Override
+    public User add(User user) {
+        user.setActivated(true);
+        user.setRole(Role.PLAYER);
+        user.setPassword(encoder.encode(user.getPassword()));
+
+        return super.add(user);
+    }
+
+    @Override
+    public User edit(User source, User changes) {
+        source.setNickname(changes.getNickname());
+        source.setEmail(changes.getEmail());
+        source.setPassword(encoder.encode(source.getPassword()));
+
+        return repo.save(source);
     }
 
     @Override
