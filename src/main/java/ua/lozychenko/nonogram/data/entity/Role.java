630a290cfc2349e80522f1cdab42077f49393ffa
Andrii Lozychenko <andrew.lozychenko@gmail.com>
2024-01-19 12:12:11 +0200

    Data: Role changes to enum

--- src/main/java/ua/lozychenko/nonogram/data/entity/Role.java
+++ src/main/java/ua/lozychenko/nonogram/data/entity/Role.java
@@ -1,40 +1,12 @@
 package ua.lozychenko.nonogram.data.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import org.springframework.security.core.GrantedAuthority;
 
-@Entity
-public class Role implements GrantedAuthority {
-    @Id
-    private Long id;
-    private String name;
-
-    public Role() {
-    }
-
-    public Role(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+public enum Role implements GrantedAuthority {
+    PLAYER, ADMIN;
 
     @Override
     public String getAuthority() {
-        return getName();
+        return name();
     }
 }
