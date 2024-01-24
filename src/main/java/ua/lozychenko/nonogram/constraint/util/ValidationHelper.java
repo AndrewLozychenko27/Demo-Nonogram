0c32e783b0deee66d791757f6368d05edbc0118f
Andrii Lozychenko <andrew.lozychenko@gmail.com>
2024-01-24 12:52:45 +0200

    Validation: added validation helper

---
+++ src/main/java/ua/lozychenko/nonogram/constraint/util/ValidationHelper.java
@@ -0,0 +1,73 @@
+package ua.lozychenko.nonogram.constraint.util;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import ua.lozychenko.nonogram.constraint.PasswordConfirmation;
+import ua.lozychenko.nonogram.constraint.StrongPassword;
+import ua.lozychenko.nonogram.constraint.UniqueEmail;
+import ua.lozychenko.nonogram.data.entity.User;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.Comparator;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ValidationHelper {
+    public static Map<String, Integer> PRIORITY = Map.of(
+            NotEmpty.class.getSimpleName(), 0,
+            Size.class.getSimpleName(), 1,
+            Length.class.getSimpleName(), 1,
+            PasswordConfirmation.class.getSimpleName(), 1,
+            Pattern.class.getSimpleName(), 2,
+            StrongPassword.class.getSimpleName(), 2,
+            UniqueEmail.class.getSimpleName(), 3
+    );
+
+    public static Map<String, String> OBJECT_TO_FIELD = Map.of(
+            User.class.getSimpleName().toLowerCase(Locale.ROOT), "passwordConfirmation"
+    );
+
+    public static BindingResult filterErrors(BindingResult source) {
+        BindingResult filtered = new BeanPropertyBindingResult(source.getTarget(), source.getObjectName());
+
+        filterFieldsErrors(source, filtered);
+        filterObjectErrors(source, filtered);
+
+        return filtered;
+    }
+
+    private static void filterFieldsErrors(BindingResult source, BindingResult filtered) {
+        Set<String> fields = source.getFieldErrors().stream()
+                .map(FieldError::getField)
+                .collect(Collectors.toSet());
+
+        if (source.hasFieldErrors()) {
+            fields.forEach(field ->
+                    filtered.addError(source.getFieldErrors(field).stream()
+                            .min(Comparator.comparing(error -> PRIORITY.get(error.getCode())))
+                            .orElseThrow(IllegalArgumentException::new))
+            );
+        }
+    }
+
+    private static void filterObjectErrors(BindingResult source, BindingResult filtered) {
+        FieldError fieldError;
+
+        if (source.hasGlobalErrors()) {
+            fieldError = source.getGlobalErrors().stream()
+                    .min(Comparator.comparing(error -> PRIORITY.get(error.getCode())))
+                    .map(error -> new FieldError(error.getObjectName(), OBJECT_TO_FIELD.get(error.getObjectName()), Objects.requireNonNull(error.getDefaultMessage())))
+                    .orElseThrow(IllegalArgumentException::new);
+            if (!filtered.hasFieldErrors(fieldError.getField())) {
+                filtered.addError(fieldError);
+            }
+        }
+    }
+}
