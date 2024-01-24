e6cb43190880b4243a7dbf9a52697e70eb1fc8ea
Andrii Lozychenko <andrew.lozychenko@gmail.com>
2024-01-24 12:52:22 +0200

    Validation: added Password confirmation validation

---
+++ src/main/java/ua/lozychenko/nonogram/constraint/PasswordConfirmation.java
@@ -0,0 +1,21 @@
+package ua.lozychenko.nonogram.constraint;
+
+import ua.lozychenko.nonogram.constraint.validator.PasswordConfirmationValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = PasswordConfirmationValidator.class)
+@Target(ElementType.TYPE_USE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PasswordConfirmation {
+    String message() default "Password confirmation must match the password";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/ua/lozychenko/nonogram/constraint/validator/PasswordConfirmationValidator.java
@@ -0,0 +1,19 @@
+package ua.lozychenko.nonogram.constraint.validator;
+
+import ua.lozychenko.nonogram.constraint.PasswordConfirmation;
+import ua.lozychenko.nonogram.data.entity.User;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordConfirmationValidator implements ConstraintValidator<PasswordConfirmation, User> {
+    @Override
+    public void initialize(PasswordConfirmation constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(User user, ConstraintValidatorContext constraintValidatorContext) {
+        return user.getPassword().equals(user.getPasswordConfirmation());
+    }
+}
