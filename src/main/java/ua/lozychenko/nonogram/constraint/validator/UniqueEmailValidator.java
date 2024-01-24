cbfde239d5852e64091d4fb706f1b951974c6a92
Andrii Lozychenko <andrew.lozychenko@gmail.com>
2024-01-24 12:52:31 +0200

    Validation: added Unique email validation

---
+++ src/main/java/ua/lozychenko/nonogram/constraint/validator/UniqueEmailValidator.java
@@ -0,0 +1,24 @@
+package ua.lozychenko.nonogram.constraint.validator;
+
+import ua.lozychenko.nonogram.constraint.UniqueEmail;
+import ua.lozychenko.nonogram.data.service.UserService;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+    private final UserService userService;
+
+    public UniqueEmailValidator(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void initialize(UniqueEmail constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        return userService.findByEmail(s).isEmpty();
+    }
+}
