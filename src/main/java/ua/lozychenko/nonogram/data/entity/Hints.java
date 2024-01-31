f1ed4af822310c82726d7b153950a1bdcc38e687
Andrii Lozychenko <andrew.lozychenko@gmail.com>
2024-01-31 13:22:06 +0200

    Data: added Hints Entity

---
+++ src/main/java/ua/lozychenko/nonogram/data/entity/Hints.java
@@ -0,0 +1,48 @@
+package ua.lozychenko.nonogram.data.entity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Hints {
+    private Map<Integer, List<Short>> vertical;
+    private Map<Integer, List<Short>> horizontal;
+
+    public Hints() {
+        this.vertical = new HashMap<>();
+        this.horizontal = new HashMap<>();
+    }
+
+    public Map<Integer, List<Short>> getVertical() {
+        return vertical;
+    }
+
+    public void setVertical(Map<Integer, List<Short>> vertical) {
+        this.vertical = vertical;
+    }
+
+    public Map<Integer, List<Short>> getHorizontal() {
+        return horizontal;
+    }
+
+    public void setHorizontal(Map<Integer, List<Short>> horizontal) {
+        this.horizontal = horizontal;
+    }
+
+    public void addVertical(int rowNum, List<Short> hint) {
+        initiateIfNull(vertical, rowNum);
+        vertical.get(rowNum).addAll(hint);
+    }
+
+    public void addHorizontal(int colNum, List<Short> hint) {
+        initiateIfNull(horizontal, colNum);
+        horizontal.get(colNum).addAll(hint);
+    }
+
+    private void initiateIfNull(Map<Integer, List<Short>> line, int index) {
+        if (!line.containsKey(index)) {
+            line.put(index, new ArrayList<>());
+        }
+    }
+}
