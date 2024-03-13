7f0be1619420b11df562c3405a977d92e69484e4
Andrii Lozychenko <andrew.lozychenko@gmail.com>
2024-03-13 12:41:25 +0200

    Added queries for puzzle generators

--- src/main/java/ua/lozychenko/nonogram/data/repo/CellRepo.java
+++ src/main/java/ua/lozychenko/nonogram/data/repo/CellRepo.java
@@ -1,12 +1,24 @@
 package ua.lozychenko.nonogram.data.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ua.lozychenko.nonogram.data.entity.Cell;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CellRepo extends JpaRepository<Cell, Long> {
     Optional<Cell> findByXAndY(short x, short y);
+
+    @Query("FROM Cell c WHERE c.x < :x AND c.y < :y")
+    List<Cell> findAllByLimit(@Param("x") short x, @Param("y") short y);
+
+    @Query("FROM Cell c WHERE c.x < :x AND c.y < :y AND c.y <= c.x")
+    List<Cell> findAllByLimitDiagonally(@Param("x") short x, @Param("y") short y);
+
+    @Query("FROM Cell c WHERE c.x < :x AND c.y < :y AND c.y < :x - c.x")
+    List<Cell> findAllByLimitDiagonallyReverse(@Param("x") short x, @Param("y") short y);
 }
