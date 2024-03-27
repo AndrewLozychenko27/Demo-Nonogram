4fd7f3633db590ee84d699e2aface4b1a5a6ba1a
Andrii Lozychenko <andrew.lozychenko@gmail.com>
2024-03-27 12:14:05 +0200

    Added puzzle generators properties

--- src/main/java/ua/lozychenko/nonogram/config/property/PuzzleGeneratorProperty.java
+++ src/main/java/ua/lozychenko/nonogram/config/property/PuzzleGeneratorProperty.java
@@ -3,5 +3,6 @@ package ua.lozychenko.nonogram.config.property;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("puzzle.generator")
-public record PuzzleGeneratorProperty(Double fillPercent, Double fillPercentRange) {
+public record PuzzleGeneratorProperty(Double fillPercent, Double fillPercentRange, Double emptyPuzzle,
+                                      Double[] thresholdMultipliers) {
 }
