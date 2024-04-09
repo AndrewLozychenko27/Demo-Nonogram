aff4421590464d1f22c939c5fd1f398c97e3e2f4
Andrii Lozychenko <andrew.lozychenko@gmail.com>
2024-04-09 10:40:24 +0300

    NotEmpty constraint changed to NotBlank

--- src/main/java/ua/lozychenko/nonogram/data/entity/Puzzle.java
+++ src/main/java/ua/lozychenko/nonogram/data/entity/Puzzle.java
@@ -13,8 +13,9 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 import ua.lozychenko.nonogram.constraint.UniquePuzzleName;
 import ua.lozychenko.nonogram.constraint.group.NameGroup;
@@ -33,8 +34,9 @@ public class Puzzle {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "puzzle_seq")
     private Long id;
 
-    @NotEmpty(message = "Puzzle name is required", groups = NameGroup.class)
+    @NotBlank(message = "Puzzle name is required", groups = NameGroup.class)
     @Length(max = 256, message = "Puzzle name must be no longer than {max} characters", groups = NameGroup.class)
+    @Pattern(regexp = "^\\w+$", message = "Puzzle name must contain only a-z, A-Z, 0-9 and _", groups = NameGroup.class)
     private String name;
 
     @NotNull(message = "Width is required", groups = SizeGroup.class)
